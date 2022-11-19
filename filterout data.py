43e6870dd59bae199a78c249130b86245877457a
amrutakamthane123 <amrutakamthane@gmail.com>
2022-11-19 09:50:40 +0530

    filterout data.py

---
+++ filterout data.py
@@ -0,0 +1,24 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+
+if __name__ == "__main__":
+    spark = SparkSession.builder.master("local[*]").appName('filter out data').getOrCreate()
+
+    Schema_data = StructType([
+        StructField('id', IntegerType()),
+        StructField('name', StringType()),
+        StructField('gender', StringType()),
+        StructField('address', StringType()),
+        StructField('salary', DoubleType())
+    ])
+
+    df = spark.read.load(r'D:\Pyspark\employee.csv', format='csv', schema=Schema_data)
+    #df.printSchema()
+    #df.show()
+    #df.filter(df.gender=='male').select('id','name','gender').show()
+    df.filter(df.gender != 'male'.select('id', 'name', 'gender').show())
+    #salary less than 10000
+    #df.filter(df.salary<10000).show()
+    #startwith endwith
+    #df.filter(df.gender.startswith('M')).show()
+    
